cb9154d8af1a9e31dc5bbf47b683ca7fcf5a91f3
Invergent Technologies <invergenttechnologies@gmail.com>
2019-11-19 09:55:42 +0100

    vla database clean state

--- src/Application/Database/seeders/20190522104217-demo-claims-statistics.js
+++ src/Application/Database/seeders/20190522104217-demo-claims-statistics.js
@@ -1,12 +1,16 @@
 module.exports = {
   up: queryInterface => queryInterface.bulkInsert('ClaimsStatistics', [{
     year: '2019',
-    Jan: '4500',
-    Feb: '4850',
-    Mar: '4100',
-    Apr: '5100',
-    May: '5600',
-    Jun: '4950',
+    Jan: 0,
+    Feb: 0,
+    Mar: 0,
+    Apr: 0,
+    May: 0,
+    Jun: 0,
+    Jul: 0,
+    Aug: 0,
+    Sep: 0,
+    Oct: 0,
     createdAt: new Date(),
     updatedAt: new Date()
   }]),
